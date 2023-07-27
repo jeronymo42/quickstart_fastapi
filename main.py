4aa29eaf0832bda00e390ed234c3da677b30eb00
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-07-27 23:47:22 +0300

    replace view, change initial function

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get('/')
 async def main_page():
-    with open('index.html') as file:
-        result = ''.join(file.readlines())
-        return HTMLResponse(content=result, status_code=200)
+        return  FileResponse('view/index.html')
