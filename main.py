d46c2d85737d112bb23332223a3de8c72657fff9
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-07-28 00:01:04 +0300

    add calculate function

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get('/')
 async def main_page():
         return  FileResponse('view/index.html')
+
+@app.post('/calculate/')
+async def calculate(num_1:int, num_2:int):
+        return {"result":num_1 + num_2}
