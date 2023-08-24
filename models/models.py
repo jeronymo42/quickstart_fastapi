40caa10d41c3d4ceffc45ca39deb6e55ca441106
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-08-24 22:04:57 +0300

    models code style refactoring

--- models/models.py
+++ models/models.py
@@ -1,20 +1,23 @@
 from pydantic import BaseModel, Field, EmailStr
-import re
+
 
 class User(BaseModel):
     name: str
     id: int
 
+
 class UserAge(BaseModel):
     name: str
     age: int
+
 
 class Feedback(BaseModel):
     name: str
     message: str
 
+
 class UserCreate(BaseModel):
-    name:str
-    email: EmailStr 
+    name: str
+    email: EmailStr
     age: int = Field(ge=0)
-    is_subscribed:bool = False
+    is_subscribed: bool = False
